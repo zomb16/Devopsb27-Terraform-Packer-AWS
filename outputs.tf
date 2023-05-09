69cd9fa9bb4ccd61ecc47367af5cf8ce0cc1b44e
Zomb <awsrameshraju@gmail.com>
2023-05-09 20:48:35 +1000

    updated output.tf with subnet

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,15 @@
 output vpc_id {
     value = aws_vpc.testvpc001.id
-    sensitive = true
+    sensitive = false
 }
 
 output vpc_arn {
     value = aws_vpc.testvpc001.arn
-    sensitive = true
+    sensitive = false
 }
+
+output subnet_1 {
+    value = aws_subnet.subnet1-public.id
+    sensitive = false
+}
+
