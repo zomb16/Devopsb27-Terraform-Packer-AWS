efea547a12fa8021996623c64fa2000e6803e2e7
Zomb <awsrameshraju@gmail.com>
2023-05-09 22:40:24 +1000

    updated file

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,25 @@
-output vpc_id {
-    value = aws_vpc.testvpc001.id
-    sensitive = false
+output "vpc_id" {
+  value     = aws_vpc.testvpc001.id
+  sensitive = false
 }
 
-output vpc_arn {
-    value = aws_vpc.testvpc001.arn
-    sensitive = false
+output "vpc_arn" {
+  value     = aws_vpc.testvpc001.arn
+  sensitive = false
 }
 
-output subnet_1 {
-    value = aws_subnet.subnet1-public.id
-    sensitive = false
+output "subnet_1" {
+  value     = aws_subnet.subnet1-public.id
+  sensitive = false
 }
 
-output subnet_2 {
-    value = aws_subnet.subnet2-public.id
-    sensitive = false
+output "subnet_2" {
+  value     = aws_subnet.subnet2-public.id
+  sensitive = false
 }
 
-output subnet_3 {
-    value = aws_subnet.subnet3-public.id
-    sensitive = false
+output "subnet_3" {
+  value     = aws_subnet.subnet3-public.id
+  sensitive = false
 }
 
